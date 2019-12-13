d46ebaf98bb0fc01caf0a8371964f7b3ad51395a
Rafa <rafa.lozano.rolon@gmail.com>
2019-12-13 12:57:15 -0600

    Se agrego 3 botones, ok,mas,menos para navegar en el menu

--- pantalla.ino
+++ pantalla.ino
@@ -1,19 +1,23 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+#define Btn_ok 2
+#define Btn_mas 3
+#define Btn_menos 4
+
 //Crear el objeto lcd  dirección  0x3F y 16 columnas x 2 filas
 LiquidCrystal_I2C lcd(0x27, 16, 2); //
 
 void setup()
 {
-    // Inicializar el LCD
-    lcd.init();
+    pinMode(Btn_ok,INPUT_PULLUP);//Se define el boton ok como entrada y se habilida pullup
+    pinMode(Btn_mas, INPUT_PULLUP); //Se define el boton ok como entrada y se habilida pullup
+    pinMode(Btn_menos, INPUT_PULLUP); //Se define el boton ok como entrada y se habilida pullup
 
-    //Encender la luz de fondo.
-    lcd.backlight();
-
-    // Escribimos el Mensaje en el LCD.
-    lcd.print("Hola Mundo");
+    lcd.init(); // Inicializar el LCD
+    lcd.backlight(); // Encender la luz de fondo.
+    lcd.setCursor(0,0);
+    lcd.print("Hola Mundo"); // Escribimos el Mensaje en el LCD.
 }
 
 void loop()
